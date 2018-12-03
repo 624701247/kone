3c1c5f8e715c4080663d16c1cfd5b934a99df072
kone <kone@hudongpai.com>
2018-12-03 18:23:38 +0800

    1

--- es6/SelAssDataMgr.js
+++ es6/SelAssDataMgr.js
@@ -52,10 +52,20 @@ export class SelAssDataMgr {
 		defAry = defAry || []
 		var curData = this.listData
 		for(var idx = 0; idx < this.blockNum; idx++) {
-			if( !(defAry[idx] && curData[defAry[idx]] != null) ) {
-				defAry[idx] = utils.getFirstKey(curData)
+			if( curData == null || (JSON.stringify(curData) == '{}') ) { //
+				defAry[idx] = ''
+				console.log('abcfd')
 			}
-			curData = curData[defAry[idx]].items
+			else {
+				if( defAry[idx] && curData[defAry[idx]] != null ) { // 默认值存在 并且 能再数据列表中找到该项
+					console.log('ddd', defAry)
+				}
+				else { //没有传默认值则取数据列表第一项
+					defAry[idx] = utils.getFirstKey(curData)
+					console.log('ccc', defAry)	
+				}
+				curData = curData[defAry[idx]].items
+			}
 		}
 
 		this.selValAry = defAry
@@ -79,10 +89,16 @@ export class SelAssDataMgr {
 	}
 
 	// 
-	getListDataById(bId) {		
+	getListDataById(bId) {	
+		console.log('bid', bId)	
 		var data = this.listData
 		for(var idx = 0; idx < bId; idx++) {
-			data = data[this.selValAry[idx]].items
+			if(this.selValAry[idx]) {
+				data = data[this.selValAry[idx]].items
+			}
+			else {
+				return {}
+			}
 		}
 		return data
 	}
